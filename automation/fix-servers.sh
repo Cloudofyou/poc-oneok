c4e628483ae820c2cc0ae89e36c7ef46b32d46ce
Glenn Wiseman <gwiseman@cumulusnetworks.com>
2019-07-15 19:23:17 -0500

    sync

--- automation/fix-servers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ansible servers -a `wget -O /home/cumulus/.ssh/authorized_keys "http://192.168.200.254/authorized_keys"`
-ansible storage -a `wget -O /home/cumulus/.ssh/authorized_keys "http://192.168.200.254/authorized_keys"`
-ansible servers -a "sudo ip route delete default"
-ansible storage -a "sudo ip route delete default"
-ansible servers -a "sudo ip route add default via 10.1.212.1"
-ansible storage -a "sudo ip route add default via 10.18.10.1" 
